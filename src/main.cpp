a4b5b015d5178e15b3280f237ba7b66de9a237b9
Vasilios Magriplis <etameson@gmail.com>
2020-02-01 14:05:55 +1100

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,22 @@
 class A
 {
 public:
-    A() : i(1) {}
-    A(int i) : i(i) {}
+    A() : i(1), t(2) {}
+    A(int i, short t) : i(i), t(t) {}
     int i;
+    short t;
 };
 
-class ObjectPool
+class MemoryPool
 {
     public:
-        ObjectPool(int size) : buffer(new char[size]), offset(0) {}
-        virtual ~ObjectPool() { delete[] buffer; }
+        MemoryPool(int size) : buffer(new char[size]), offset(0) {}
+        virtual ~MemoryPool() { delete[] buffer; }
 
         template<typename T, typename...Args>
         T* Allocate(Args&&...args)
         {
+            std::cout << "alignment: " << std::alignment_of<T>::value << " sizeof: " << sizeof(T) << std::endl;
             offset += sizeof(T);
             return new(buffer+offset) T(std::forward<Args>(args)...);
         }
@@ -29,10 +31,12 @@ class ObjectPool
 
 int main()
 {
-    ObjectPool op(10000);
-    A* a = op.Allocate<A>(1);
-    A* b = op.Allocate<A>(2);
+    MemoryPool op(10000);
+    A* a = op.Allocate<A>(1, 5);
+    A* b = op.Allocate<A>(2, 7);
+    op.Allocate<int>(1);
 
-    std::cout << b->i << " " << a->i << std::endl;
+    std::cout << sizeof(wchar_t) << std::alignment_of<wchar_t>::value << std::endl;
+    std::cout << b->i << " " << a->t << std::endl;
 }
 
