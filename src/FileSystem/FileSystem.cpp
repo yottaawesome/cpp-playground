670334704f22dcf4bbbcbbafcc71db2550ca50b2
Vasilios Magriplis <etameson@gmail.com>
2021-03-10 20:48:00 +1100

    Finally fix DllModule

--- src/FileSystem/FileSystem.cpp
+++ src/FileSystem/FileSystem.cpp
@@ -5,8 +5,16 @@ int main(int argc, char* args[])
 {
     std::filesystem::path currentPath = std::filesystem::current_path();
     currentPath = currentPath.parent_path();
-
     std::wcout << currentPath.c_str() << std::endl;
+
+    currentPath = std::filesystem::current_path();
+    currentPath = "..";
+    //std::filesystem::current_path();
+    //currentPath += "\\..";
+    std::wcout << std::filesystem::absolute(currentPath).c_str() << std::endl;
+
+    std::filesystem::path testPath = "C:\\Temp\\Temp2";
+    std::filesystem::create_directories(testPath);
 
     return 0;
 }
