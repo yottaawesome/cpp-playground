8a97094e7c6ac7c4957ccf9244a577a64ec2a03c
Vasilios Magriplis <etameson@gmail.com>
2022-08-07 07:42:56 +1000

    String templates

---
+++ src/StringTemplates/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+// See https://stackoverflow.com/questions/2033110/passing-a-string-literal-as-a-type-argument-to-a-class-template
+template<unsigned N>
+struct FixedString 
+{
+	char buf[N + 1]{};
+	constexpr FixedString(char const* s) 
+	{
+		for (unsigned i = 0; i < N; i++) 
+			buf[i] = s[i];
+	}
+	constexpr operator char const* () const 
+	{ 
+		return buf; 
+	}
+};
+template<unsigned N> FixedString(char const (&)[N])->FixedString<N - 1>;
+
+template<FixedString T>
+class Foo 
+{
+	static constexpr char const* Name = T;
+	public:
+		void hello() const 
+		{
+			std::cout << Name << std::endl;
+		};
+};
+
+template <size_t N>
+struct FixedString2
+{
+	char buf[N]{};
+	constexpr FixedString2(const char(&arg)[N])
+	{
+		for (unsigned i = 0; i < N; i++)
+			buf[i] = arg[i];
+	}
+
+	constexpr operator char const* () const
+	{
+		return buf;
+	}
+
+	static void Blah() 
+	{
+		std::cout << buf << std::endl;
+	}
+};
+template<size_t N> FixedString2(char const (&)[N])->FixedString2<N>;
+
+template<FixedString2 T>
+class Foo2 
+{
+	public:
+		static constexpr const char* Name = T;
+		void hello() const
+		{
+			std::cout << Name << std::endl;;
+		};
+};
+
+int main(int argc, char* argv[])
+{
+	Foo<"Hello!"> foo;
+	foo.hello();
+
+	FixedString s = "AA";
+	FixedString2 s2 = "AA";
+
+	Foo2<"P"> p;
+	Foo2<"O"> o;
+	p.hello();
+	o.hello();
+	//H a = "A";
+	//H b = L"A";
+	//H c = L"A";
+
+    return 0;
+}
