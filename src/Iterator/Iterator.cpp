a23c7eeabe93e4a11f099724bfe62897f8359bc9
Vasilios Magriplis <etameson@gmail.com>
2023-02-21 06:33:31 +1100

    Add iterator sample

--- src/Iterator/Iterator.cpp
+++ src/Iterator/Iterator.cpp
@@ -1,57 +1,46 @@
+// Adapted from https://www.modernescpp.com/index.php?option=com_content&view=article&id=683&catid=58
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <functional>
-#include <Windows.h>
 
-class A
+class Generator 
 {
-	public:
-		virtual ~A() {}
-		virtual void DoX() { std::cout << "OK 1" << std::endl; }
+    int begin_{};
+    int end_{};
+
+    public:
+        Generator(int begin, int end) : begin_{ begin }, end_{ end } {}
+
+        class Iterator 
+        {
+            int value_{};
+
+            public:
+                explicit Iterator(int pos) : value_{ pos } {}
+
+                int operator*() const { return value_; }           // (3)
+
+                Iterator& operator++() 
+                {                           // (4)
+                    ++value_;
+                    return *this;
+                }
+
+                bool operator!=(const Iterator& other) const 
+                {      // (5)
+                    return value_ != other.value_;
+                }
+        };
+
+        Iterator begin() const { return Iterator{ begin_ }; }     // (1)
+        Iterator end() const { return Iterator{ end_ }; }         // (2)
 };
 
-class B : public A
-{
-	public:
-		virtual ~B() {}
-		virtual void DoX() override { std::cout << "OK 2" << std::endl; }
-};
+int main() {
 
-/*
-template<typename T>
-void FindIn(std::vector<T>& results, const std::vector<T>& search, const std::function<bool(const T&)>& predicate)
-{
-	auto iter = search.begin();
+    std::cout << '\n';
 
-	while (iter != search.end())
-	{
-		iter = std::find_if(iter, search.end(), predicate);
-		if (iter != search.end())
-		{
-			results.push_back(*iter);
-			iter++;
-		}
-	}
-}
+    Generator gen{ 100, 110 };
+    for (auto v : gen) std::cout << v << " ";
 
-void TestIterators()
-{
-	SYSTEMTIME st;
-	GetSystemTime(&st);
-	std::cout << st.wHour << std::endl;
-
-	std::vector<int> search{ 0,1,5,0,5,9,4 };
-	std::vector<int> results;
-	FindIn<int>(results, search, [](const int& value) -> bool { return value == 5; });
-	for (const int& i : results)
-		std::cout << i << std::endl;
-}
-*/
-
-int main()
-{
-	//testVector.push_back(B());
-
-	return 0;
+    std::cout << "\n\n";
 }
