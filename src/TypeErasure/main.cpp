d52f436716fa73ee3abb0ae6b87704dfcc38f4e4
Vasilios Magriplis <etameson@gmail.com>
2023-04-24 14:57:28 +1000

    Clean up TypeErasure

--- src/TypeErasure/main.cpp
+++ src/TypeErasure/main.cpp
@@ -6,59 +6,66 @@
 #include <string>
 #include <vector>
 
-class Object {                                              // (2)
+class Object 
+{                                              // (2)
+    public:
+        template <typename T>                                   // (3)
+        Object(T&& obj) : object(std::make_shared<Model<T>>(std::forward<T>(obj))) {}
 
-public:
-    template <typename T>                                   // (3)
-    Object(T&& obj) : object(std::make_shared<Model<T>>(std::forward<T>(obj))) {}
-
-    std::string getName() const {                           // (4)
-        return object->getName();
-    }
-
-    struct Concept {                                         // (5)
-        virtual ~Concept() {}
-        virtual std::string getName() const = 0;
-    };
-
-    template< typename T >                                   // (6)
-    struct Model : Concept {
-        Model(const T& t) : object(t) {}
-        std::string getName() const override {
-            return object.getName();
+        std::string getName() const 
+        {                           // (4)
+            return object->getName();
         }
-    private:
-        T object;
-    };
 
-    std::shared_ptr<const Concept> object;
+        struct Concept 
+        {                                         // (5)
+            virtual ~Concept() = default;
+            virtual std::string getName() const = 0;
+        };
+
+        template<typename T>                                   // (6)
+        struct Model : Concept 
+        {
+            Model(const T& t) : object(t) {}
+            std::string getName() const override 
+            {
+                return object.getName();
+            }
+            private:
+                T object;
+        };
+
+        std::shared_ptr<const Concept> object;
 };
 
-
-void printName(std::vector<Object> vec) {                    // (7)
-    for (auto v : vec) std::cout << v.getName() << '\n';
+void printName(const std::vector<Object>& vec) 
+{                    // (7)
+    for (const auto& v : vec) 
+        std::cout << v.getName() << '\n';
 }
 
-struct Bar {
-    std::string getName() const {                           // (8)
+struct Bar 
+{
+    std::string getName() const 
+    {                           // (8)
         return "Bar";
     }
 };
 
-struct Foo {
-    std::string getName() const {                           // (8)
+struct Foo 
+{
+    std::string getName() const 
+    {                           // (8)
         return "Foo";
     }
 };
 
-int main() {
-
+int main() 
+{
     std::cout << '\n';
-
     std::vector<Object> vec{ Object(Foo()), Object(Bar()) };  // (1)
-
     printName(vec);
-
     std::cout << '\n';
 
+    return 0;
 }
