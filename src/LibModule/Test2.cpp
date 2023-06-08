c1437eac7b248c43654d1c2715cdfdc6d7b7ba08
Vasilios Magriplis <etameson@gmail.com>
2023-06-08 19:45:48 +1000

    Remove unnecessary pch files

--- src/LibModule/Test2.cpp
+++ src/LibModule/Test2.cpp
@@ -1,7 +1,5 @@
 module; // everything below is part of the implicit global module fragment
 
-#include "pch.h"
-#include "framework.h"
 #include <iostream>
 
 module LibModule; // everything below belongs to LibModule module
