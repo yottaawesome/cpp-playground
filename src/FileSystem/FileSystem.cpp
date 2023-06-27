d18a55ed78c6e7c4809ae7987eca8bd5bcd675e7
Vasilios Magriplis <etameson@gmail.com>
2023-06-28 06:16:29 +1000

    Messing around with paths

--- src/FileSystem/FileSystem.cpp
+++ src/FileSystem/FileSystem.cpp
@@ -16,5 +16,16 @@ int main(int argc, char* args[])
     std::filesystem::path testPath = "C:\\Temp\\Temp2";
     std::filesystem::create_directories(testPath);
 
+    std::filesystem::path testPath2 = "C:/Temp/Temp2/Temp3";
+    for (auto& x : testPath2)
+    {
+        std::cout << x << " ";
+    }
+    std::filesystem::path testPath3 = "Temp\\Temp2\\Temp3";
+    for (auto& x : testPath3)
+    {
+        std::cout << x << " ";
+    }
+
     return 0;
 }
