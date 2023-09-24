361f75c98391bfc0fa29146108436c21e9c42cbb
Vasilios Magriplis <crownmonarch@hotmail.com>
2023-09-24 21:53:05 +0930

    Minor changes

--- src/ExecutableModule/TestModule-InternalPartition.cpp
+++ src/ExecutableModule/TestModule-InternalPartition.cpp
@@ -1,4 +1,5 @@
 // MSVC is retarded and requires manually setting the compile option
 // for internal partition units to work correctly.
 // See https://developercommunity.visualstudio.com/t/Cannot-import-partition-implementation-u/10405337
+// and https://developercommunity.visualstudio.com/t/C20-Modules-Module-Partition-Impleme/1634347
 module TestModule:InternalPartition;
