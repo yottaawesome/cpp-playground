1bcb9509a3454d1f6f79b3655ed25bd60391af61
Vasilios Magriplis <crownmonarch@hotmail.com>
2023-10-01 03:46:21 +1100

    Minor change

--- src/ExecutableModule/TestModule-InternalPartition.cpp
+++ src/ExecutableModule/TestModule-InternalPartition.cpp
@@ -2,4 +2,13 @@
 // for internal partition units to work correctly.
 // See https://developercommunity.visualstudio.com/t/Cannot-import-partition-implementation-u/10405337
 // and https://developercommunity.visualstudio.com/t/C20-Modules-Module-Partition-Impleme/1634347
+// The main difference between internal module partitions and module
+// partitions is that internal module partitions do not have the 
+// export keyword and cannot export any symbols.
 module TestModule:InternalPartition;
+
+// Stuff in here is only visible inside the module
+namespace NotExported
+{
+
+}
