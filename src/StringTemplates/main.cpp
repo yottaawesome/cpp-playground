1f825a6836c8bdfaa5b26d84bf69fef85dc75ab1
Vasilios Magriplis <etameson@gmail.com>
2023-10-20 08:27:22 +1100

    Minor fooling around with concepts

--- src/StringTemplates/main.cpp
+++ src/StringTemplates/main.cpp
@@ -1,7 +1,22 @@
 import std;
 import stringtemplates;
 
+template<typename X>
+concept x = requires(X y)
+{
+    X::What();
+    y.What2();
+};
+
+struct S
+{
+    static void What() {}
+    void What2() {}
+};
+static_assert(x<S>);
+
 int main(int argc, char* argv[])
 {
+    S s;
     return 0;
 }
