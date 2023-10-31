fa3b5ba5e2126d92d0da2295fdb53a0b10de0428
Vasilios Magriplis <etameson@gmail.com>
2023-10-31 21:12:31 +1100

    Minor changes

--- src/EliminatingVirtual/main.cpp
+++ src/EliminatingVirtual/main.cpp
@@ -2,164 +2,211 @@
 // https://github.com/CppCon/CppCon2022/blob/main/Presentations/Using-Modern-C-to-Eliminate-Virtual-Functions-Jonathan-Gopel-CppCon-2022.pdf
 
 import std;
+import std.compat;
 
-class A { };
-
-void GH() noexcept {}
-
-// https://stackoverflow.com/questions/44269678/stdis-nothrow-invocable-with-member-function
-// https://mariusbancila.ro/blog/2022/06/20/requires-expressions-and-requires-clauses-in-cpp20/
-// https://www.fluentcpp.com/2020/09/11/replacing-crtp-static-polymorphism-with-concepts/
-template<typename T>
-concept No = noexcept(std::declval<T>().Another());
-
-template<typename T>
-concept No2 = noexcept(T());
-
-template<typename T>
-concept IInterface = No<T> && requires(T t, const T m, const A a)
+namespace ConceptFooleryA
 {
-    typename T::IsType;
-    {t.Blah()} -> std::same_as<int>;
-    {t.AnotherBlah()} -> std::same_as<std::string>;
-    //{std::declval<const T&>().AnotherBlah()} -> std::same_as<std::string>;
-    {m.AnotherBlah()} -> std::same_as<std::string>; // Same as above
-    {t.Something(a)} -> std::same_as<void>;
-    {m.Another3(1)} noexcept -> std::same_as<void>;
-    //{noexcept(std::declval<T>().Another2())} -> true;
-    requires noexcept(t.Another());
-    requires noexcept(std::declval<T>().Another2(1));
-    requires std::is_nothrow_invocable_v<decltype(&T::Another2), T, int>;
-};
+    template<auto V>
+    struct S : std::false_type {};
 
-template<typename T>
-constexpr bool DoesSignatureMatch(auto func)
-{
-    return std::same_as<decltype(func), T>;
-}
+    template<>
+    struct S<1> : std::true_type {};
 
-template<typename T>
-concept IInterface2 = requires(T t)
-{
-    // Exact match on argument parameters, whereas {t.Another3(long long{})} -> ...
-    // will pass on functions with narrowing narrower types
-    requires std::same_as<decltype(&T::Another3), void(T::*)(int)const noexcept>;
-    requires std::same_as<decltype(&T::Another3), auto(T::*)(int)const noexcept -> void>;
-    requires DoesSignatureMatch<void(T::*)(int)const noexcept>(&T::Another3);
-};
+    template<auto T>
+    concept IsS = S<T>::value and T == 1; // deliberately redundant
 
-template <typename T>
-constexpr bool always_false = std::false_type::value;
-
-template<typename T>
-void SomeTest(T t)
-{
-    if constexpr (std::same_as<T, int>)
+    template<typename T>
+    concept IsS2 = requires(T t)
     {
+        true;
+    };
 
-    }
-    else
+    template<auto T>
+    concept IsS3 = requires() { true; };
+
+    template<typename T>
+    concept SomeTest = std::is_null_pointer_v<T> or std::is_invocable_v<T>;
+
+    template<SomeTest auto L = nullptr>
+    struct Moo {};
+
+    template<IsS2 auto V>
+        requires IsS<V>
+    void SomeFunc() { }
+
+    void Test()
     {
-        // https://mariusbancila.ro/blog/2022/06/20/requires-expressions-and-requires-clauses-in-cpp20/
-        static_assert(always_false<T>, "Nah");
+        SomeFunc<1>();
+
+        Moo<nullptr> x;
+        Moo<[]{}> y;
     }
 }
 
-class SomeClass
+namespace ConceptFooleryB
 {
-    public:
-        using IsType = int;
-        int Blah() { return 1; }
-        std::string AnotherBlah() const { return "Blah"; }
-        void Something(A i) {}
-        void Another() noexcept { }
-        void Another2(int i) noexcept { }
-        void Another3(int i) const noexcept{ }
-};
+    class A { };
 
-static_assert(IInterface<SomeClass>, "Must conform to the requirements of the IInterface concept");
-static_assert(IInterface2<SomeClass>, "Must conform to the requirements of the IInterface concept");
+    void GH() noexcept {}
 
-void Func(IInterface auto& obj)
-{
-    obj.Blah();
-}
+    // https://stackoverflow.com/questions/44269678/stdis-nothrow-invocable-with-member-function
+    // https://mariusbancila.ro/blog/2022/06/20/requires-expressions-and-requires-clauses-in-cpp20/
+    // https://www.fluentcpp.com/2020/09/11/replacing-crtp-static-polymorphism-with-concepts/
+    template<typename T>
+    concept No = noexcept(std::declval<T>().Another());
 
-void Func2(IInterface2 auto& obj)
-{
-    obj.Another3();
-}
+    template<typename T>
+    concept No2 = noexcept(T());
 
-template<typename T, typename... Ts>
-concept same_as_any = (... or std::same_as<T, Ts>);
+    template<typename T>
+    concept IInterface = No<T> && requires(T t, const T m, const A a)
+    {
+        typename T::IsType;
+        {t.Blah()} -> std::same_as<int>;
+        {t.AnotherBlah()} -> std::same_as<std::string>;
+        //{std::declval<const T&>().AnotherBlah()} -> std::same_as<std::string>;
+        {m.AnotherBlah()} -> std::same_as<std::string>; // Same as above
+        {t.Something(a)} -> std::same_as<void>;
+        {m.Another3(1)} noexcept -> std::same_as<void>;
+        //{noexcept(std::declval<T>().Another2())} -> true;
+            requires noexcept(t.Another());
+            requires noexcept(std::declval<T>().Another2(1));
+            requires std::is_nothrow_invocable_v<decltype(&T::Another2), T, int>;
+    };
 
-template<IInterface... TFoos>
-class Bar
-{
-    public:
-        constexpr Bar(same_as_any<TFoos...> auto input)
-            : foo(input)
-        { }
+    template<typename T>
+    constexpr bool DoesSignatureMatch(auto func)
+    {
+        return std::same_as<decltype(func), T>;
+    }
 
-        constexpr void set_foo(same_as_any<TFoos...> auto input)
+    template<typename T>
+    concept IInterface2 = requires(T t)
+    {
+        // Exact match on argument parameters, whereas {t.Another3(long long{})} -> ...
+        // will pass on functions with narrowing narrower types
+        requires std::same_as<decltype(&T::Another3), void(T::*)(int)const noexcept>;
+        requires std::same_as<decltype(&T::Another3), auto(T::*)(int)const noexcept -> void>;
+        requires DoesSignatureMatch<void(T::*)(int)const noexcept>(&T::Another3);
+    };
+
+    template <typename T>
+    constexpr bool always_false = std::false_type::value;
+
+    template<typename T>
+    void SomeTest(T t)
+    {
+        if constexpr (std::same_as<T, int>)
         {
-            foo = input;
+
         }
+        else
+        {
+            // https://mariusbancila.ro/blog/2022/06/20/requires-expressions-and-requires-clauses-in-cpp20/
+            static_assert(always_false<T>, "Nah");
+        }
+    }
+
+    class SomeClass
+    {
+        public:
+            using IsType = int;
+            int Blah() { return 1; }
+            std::string AnotherBlah() const { return "Blah"; }
+            void Something(A i) {}
+            void Another() noexcept { }
+            void Another2(int i) noexcept { }
+            void Another3(int i) const noexcept { }
+    };
+
+    static_assert(IInterface<SomeClass>, "Must conform to the requirements of the IInterface concept");
+    static_assert(IInterface2<SomeClass>, "Must conform to the requirements of the IInterface concept");
+
+    void Func(IInterface auto& obj)
+    {
+        obj.Blah();
+    }
+
+    void Func2(IInterface2 auto& obj)
+    {
+        obj.Another3();
+    }
+
+    template<typename T, typename... Ts>
+    concept same_as_any = (... or std::same_as<T, Ts>);
+
+    template<IInterface... TFoos>
+    class Bar
+    {
+        public:
+            constexpr Bar(same_as_any<TFoos...> auto input)
+                : foo(input)
+            { }
+
+            constexpr void set_foo(same_as_any<TFoos...> auto input)
+            {
+                foo = input;
+            }
 
     private:
         std::variant<TFoos...> foo{};
-};
+    };
 
-template<IInterface TFoos>
-class Bar2
-{
-public:
-    constexpr Bar2(IInterface auto input)
-        : foo(input)
-    { }
-
-    constexpr void set_foo(IInterface auto input)
+    template<IInterface TFoos>
+    class Bar2
     {
-        foo = input;
+        public:
+            constexpr Bar2(IInterface auto input)
+                : foo(input)
+            { }
+
+            constexpr void set_foo(IInterface auto input)
+            {
+                foo = input;
+            }
+
+        private:
+            TFoos foo{};
+    };
+
+    template<IInterface...TFoos>
+    class Baz
+    {
+        public:
+            auto store(same_as_any<TFoos...> auto value)
+            {
+                return std::get<std::vector<decltype(value)>>(data).push_back(value);
+            }
+
+            template<same_as_any<TFoos...> T>
+            auto store2(T value)
+            {
+                return std::get<std::vector<T>>(data).push_back(value);
+            }
+
+        private:
+            std::tuple<std::vector<TFoos>...> data{};
+    };
+
+    void Test()
+    {
+        SomeTest(1);
+        SomeClass c;
+        Func(c);
+
+        if (true xor false)
+        {
+
+        }
+
+        Bar<SomeClass> bar(c);
+        static_assert(No<SomeClass>);
     }
-
-private:
-    TFoos foo{};
-};
-
-template<IInterface...TFoos>
-class Baz
-{
-    public:
-        auto store(same_as_any<TFoos...> auto value)
-        {
-            return std::get<std::vector<decltype(value)>>(data).push_back(value);
-        }
-
-        template<same_as_any<TFoos...> T>
-        auto store2(T value)
-        {
-            return std::get<std::vector<T>>(data).push_back(value);
-        }
-
-    private:
-        std::tuple<std::vector<TFoos>...> data{};
-};
+}
 
 int main()
 {
-    SomeTest(1);
-    SomeClass c;
-    Func(c);
-
-    if (true xor false)
-    {
-
-    }
-
-    Bar<SomeClass> bar(c);
-    static_assert(No<SomeClass>);
-
+    ConceptFooleryB::Test();
     return 0;
 }
 
---
+++ src/Timing/main.cpp
@@ -0,0 +1,264 @@
+#include <windows.h>
+import std;
+import std.compat;
+
+namespace Timing
+{
+    
+    namespace CheckDuration
+    {
+        
+
+        // Adapted from https://stackoverflow.com/a/41851068/7448661
+        template<class T>
+        struct is_duration : std::false_type {};
+
+        template<class Rep, class Period>
+        struct is_duration<std::chrono::duration<Rep, Period>> : std::true_type {};
+
+        template<typename T>
+        concept Duration = is_duration<T>::value;
+
+        void SomeFunctionThatAcceptsDurations(Duration auto s)
+        {
+            static_assert(is_duration<decltype(s)>::value);
+        }
+
+        void Check()
+        {
+            SomeFunctionThatAcceptsDurations(std::chrono::milliseconds(2));
+        }
+    }
+
+    void TimeFunctionCallStd()
+    {
+        auto someLambda =
+            [] {
+            for (int i = 0; i < 10; i++)
+                std::cout << std::format("{}\n", i);
+            };
+        const auto begin = std::chrono::high_resolution_clock::now();
+        someLambda();
+        const auto end = std::chrono::high_resolution_clock::now();
+        const auto diff = end - begin;
+        std::cout << std::format(
+            "{} microseconds = {} nanoseconds\n",
+            std::chrono::duration_cast<std::chrono::microseconds>(diff).count(),
+            std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count()
+        );
+    }
+
+    void TimeFunctionCallsWindows()
+    {
+        LARGE_INTEGER l1{ 0 };
+        LARGE_INTEGER l2{ 0 };
+        QueryPerformanceFrequency(&l1);
+        uint64_t frequency = l1.QuadPart;
+
+        QueryPerformanceCounter(&l1);
+        Sleep(1000);
+        QueryPerformanceCounter(&l2);
+        uint64_t firstPoint = l1.QuadPart;
+        uint64_t secondPoint = l2.QuadPart;
+        uint64_t difference = secondPoint - firstPoint;
+        std::cout << std::format("{} seconds\n", (double)difference / frequency);
+    }
+
+    void NanoSecondIntervals()
+    {
+        using HundredNanoSecondIntervals = std::chrono::duration<long long, std::ratio<1, 10'000'000>>;
+        std::chrono::seconds twoSeconds(2);
+        HundredNanoSecondIntervals converted = std::chrono::duration_cast<HundredNanoSecondIntervals>(twoSeconds);
+        std::cout << std::format("{} 100-ns intervals in {} seconds", converted.count(), twoSeconds.count());
+    }
+
+    template <size_t N>
+    struct FixedString
+    {
+        char buf[N]{};
+        //consteval FixedString(char const* arg) noexcept
+        consteval FixedString(const char(&arg)[N]) noexcept
+        {
+            for (unsigned i = 0; i < N; i++)
+                buf[i] = arg[i];
+            //std::copy_n(arg, N, buf);
+        }
+
+        constexpr operator const char* () const noexcept // returns a random pointer when consteval, probable msvc bug
+        {
+            return buf;
+        }
+
+        consteval size_t Size() const noexcept { return N; }
+
+        consteval bool Empty() const noexcept
+        {
+            return N == 1;
+        }
+
+        constexpr const char* Data() const noexcept
+        {
+            return buf;
+        }
+
+        consteval std::string_view View() const noexcept { return { buf, N }; }
+    };
+    template<size_t N>
+    FixedString(char const (&)[N]) -> FixedString<N>;
+    //template<size_t N>
+    //FixedString(char const (&)[N]) -> FixedString<N-1>;
+
+    template<auto T, bool Enabled = true, bool ThrowErrors = true, FixedString FSComment = "">
+    struct TimedScope
+    {
+        inline void operator()(auto&&...args)
+            requires ThrowErrors
+        {
+            if constexpr (Enabled)
+            {
+                const auto begin = std::chrono::high_resolution_clock::now();
+                T(std::forward<decltype(args)>(args)...);
+                const auto end = std::chrono::high_resolution_clock::now();
+                diff = end - begin;
+                if (not FSComment.Empty())
+                {
+                    std::cout << std::format(FSComment.Data(), ToMicroseconds().count());
+                }
+            }
+            else
+            {
+                T(std::forward<decltype(args)>(args)...);
+            }
+        }
+
+        inline void operator()(auto&&...args) noexcept(Enabled) // don't inadvertently swallow exceptions when test is disabled
+            requires not ThrowErrors
+        {
+            if constexpr (Enabled)
+            {
+                std::chrono::high_resolution_clock::time_point end;
+                const auto begin = std::chrono::high_resolution_clock::now();
+                try
+                {
+                    T(std::forward<decltype(args)>(args)...);
+                    end = std::chrono::high_resolution_clock::now();
+                }
+                catch (...)
+                {
+                    end = std::chrono::high_resolution_clock::now();
+                }
+                diff = end - begin;
+                if (not FSComment.Empty())
+                {
+                    std::string_view v{ FSComment.Data(), FSComment.Size() };
+                    std::cout << std::format("{}, Microseconds: {}\n", (const char*)FSComment, ToMicroseconds().count());
+                }
+            }
+            else
+            {
+                T(std::forward<decltype(args)>(args)...);
+            }
+        }
+
+            inline std::chrono::high_resolution_clock::duration Get() const noexcept
+        {
+            return diff;
+        }
+
+        inline std::chrono::microseconds ToMicroseconds() const noexcept
+        {
+            return std::chrono::duration_cast<std::chrono::microseconds>(diff);
+        }
+
+        inline std::chrono::nanoseconds ToNanoseconds() const noexcept
+        {
+            return std::chrono::duration_cast<std::chrono::nanoseconds>(diff);
+        }
+
+        template<typename TDuration>
+        inline TDuration To() const noexcept
+        {
+            return std::chrono::duration_cast<TDuration>(diff);
+        }
+
+    private:
+        std::chrono::high_resolution_clock::duration diff;
+    };
+
+    void SomeFunc() {}
+    using Blah = TimedScope<SomeFunc>; // with C++23's static operator(), it may be possible to alias functions this way
+    Blah X;
+
+    void TestTimedScope()
+    {
+        SYSTEMTIME st{ 0 };
+        TimedScope<GetSystemTime> ts;
+        ts(&st);
+        std::cout << std::format(
+            "Call took {} microseconds = {} nanoseconds\n",
+            ts.ToMicroseconds().count(),
+            ts.ToNanoseconds().count()
+        );
+    }
+
+    void TestTimedScopeLambda()
+    {
+        constexpr auto func = []
+            {
+                SYSTEMTIME st{ 0 };
+                GetSystemTime(&st);
+            };
+
+        TimedScope<func> ts;
+        ts();
+        std::cout << std::format(
+            "Call took {} microseconds = {} nanoseconds\n",
+            ts.ToMicroseconds().count(),
+            ts.ToNanoseconds().count()
+        );
+    }
+
+    void TestTimedScopeThrowingLambda()
+    {
+        constexpr auto func =
+            [] {
+            throw std::runtime_error("Some error");
+            };
+
+        TimedScope<func, true, false, "Throwing call is"> ts;
+        ts();
+        std::cout << std::format(
+            "Throwing call took {} microseconds = {} nanoseconds\n",
+            ts.ToMicroseconds().count(),
+            ts.ToNanoseconds().count()
+        );
+    }
+
+    struct ConstTest
+    {
+        constexpr ConstTest(int s) : S(s)
+        {
+
+        }
+
+        constexpr int Get() const noexcept { return S; }
+
+        int S;
+    };
+}
+
+int main()
+{
+    constexpr Timing::ConstTest t(9);
+    constexpr int m = t.Get();
+
+    //Timing::X();
+    //Timing::TestTimedScope();
+    //Timing::TestTimedScopeLambda();
+    Timing::TestTimedScopeThrowingLambda();
+    //Timing::TimeFunctionCallStd();
+    //Timing::NanoSecondIntervals();
+    //Timing::TimeFunctionCallsWindows();
+
+    return 0;
+}
