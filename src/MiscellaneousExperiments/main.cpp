50861310a2dbb131d09403e45bb2732c43606bbc
Vasilios Magriplis <etameson@gmail.com>
2023-11-02 08:09:34 +1100

    Friends let friends experiment with their friends

--- src/MiscellaneousExperiments/main.cpp
+++ src/MiscellaneousExperiments/main.cpp
@@ -2,7 +2,45 @@
 import std;
 import std.compat;
 
+template<typename T>
+class Q
+{
+    friend T;
+
+    public:
+        Q(T& t) : m_t(t) {}
+
+    public:
+        void DoThis() { m_t.DoSomething(*this); }
+
+    private:
+        int M;
+        T m_t;
+};
+
+template<auto T>
+class Q2
+{
+    friend decltype(T);
+
+    public:
+        void DoThis() { T.DoSomething(*this); }
+
+    private:
+        int M;
+};
+
+class M
+{
+    void DoSomething(Q<M>& m)
+    {
+        m.M = 1;
+    }
+};
+
 int main()
-{    
+{
+    M m;
+    Q<M> qm(m);
     return 0;
 }
