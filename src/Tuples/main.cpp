115b9927ee6a0e52028ae416a467dca8d6dab27c
Vasilios Magriplis <etameson@gmail.com>
2024-01-19 06:19:25 +1100

    Minor changes

--- src/Tuples/main.cpp
+++ src/Tuples/main.cpp
@@ -23,39 +23,7 @@ auto make(Args&&...args) -> std::tuple<Args...>
 	return { args... };
 }
 
-// https://andreasfertig.blog/2024/01/cpp20-concepts-applied/
-namespace BitfieldEnums
-{
-	template<typename T>
-	concept op_or = std::is_enum_v<T> and requires(T e)
-	{
-		//look for enable_bitmask_operator_or to enable this operator
-		enable_bitmask_operator_or(e);
-	};
 
-	template<typename T>
-	requires op_or<T>
-	constexpr auto operator|(const T lhs, const T rhs)
-	{
-		return static_cast<T>(std::to_underlying(lhs) | std::to_underlying(rhs));
-	}
-
-	namespace Filesystem 
-	{
-		enum class Permission : uint8_t 
-		{
-			Read = 0x01,
-			Write = 0x02,
-			Execute = 0x04,
-		};
-
-		//Opt - in for operator|
-		consteval void enable_bitmask_operator_or(Permission);
-	}
-
-	using Filesystem::Permission;
-	Permission readAndWrite{ Permission::Read | Permission::Write };
-}
 
 int main()
 {
