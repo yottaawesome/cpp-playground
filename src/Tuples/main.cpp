60eabaa1da1760b9ff2eda5735c76ae4d6bf191e
Vasilios Magriplis <etameson@gmail.com>
2024-01-20 19:21:27 +1100

    Minor changes

--- src/Tuples/main.cpp
+++ src/Tuples/main.cpp
@@ -23,10 +23,96 @@ auto make(Args&&...args) -> std::tuple<Args...>
 	return { args... };
 }
 
+struct db { void save() {} void get() {} };
+struct file { void save() {} void get() {} };
 
+template<typename T>
+concept sinkable = requires(T t)
+{
+	t.get();
+	t.save();
+};
+
+struct saver
+{
+	saver(int x)
+	{
+		if (x == 1)
+			sinks = db{};
+		else
+			sinks = file{};
+	}
+
+	void save()
+	{
+		std::visit(
+			[](sinkable auto&& t)
+			{
+				t.save();
+			},
+			sinks
+		);
+		//sinks.visit([](sinkable auto&& t) { t.save(); });
+	}
+
+	void get()
+	{
+		std::visit(
+			[](sinkable auto&& t)
+			{
+				t.get();
+			},
+			sinks
+		);
+	}
+
+	std::variant<db, file> sinks;
+};
+
+struct A {};
+struct B {};
+
+//template<size_t T>
+//std::index_sequence<T> Get2(size_t t)
+//{
+//		return t == T ? std::make_index_sequence<T>{} : Get2<T + 1>(t);
+//}
+
+void Get()
+{
+	//Get2<2>(1);
+	int x = 1;
+
+	std::vector v{ 0,1,2,3,4 };
+	auto f = []<size_t...I>(size_t index, std::index_sequence<I...>) -> decltype(auto)
+	{
+		//return ((index == I ? [] {} : [] {}) and ...);
+		//return ((index == I ? [] {return A{}; }() : [] { return B{}; }()), ...);
+		//return ((index == I ? A{} : (B{})) and ...);
+
+		return ([]<size_t T = I>()
+		{
+			if constexpr (T == 1)
+			{
+				return A{};
+			}
+			else
+			{
+				return B{};
+			}
+		}(), ...);
+	}(x, std::make_index_sequence<2>{});
+
+	std::cout << typeid(f).name() << std::endl;
+}
 
 int main()
 {
+	Get();
+	saver s(1);
+	s.save();
+	s.get();
+
 	MakeSafe::Run();
 	//LoopOverTypes::Run();
 	//Chained::Run();
