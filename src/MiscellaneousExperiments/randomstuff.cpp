b0f291b350a1a65cd21334d276ff118e2851db66
Vasilios Magriplis <etameson@gmail.com>
2024-02-17 15:12:42 +1100

    Messing with lambdas

--- src/MiscellaneousExperiments/randomstuff.cpp
+++ src/MiscellaneousExperiments/randomstuff.cpp
@@ -24,8 +24,23 @@ namespace DeducingThis
         return "message";
     }
 
+    // parameter lambdas
+    int x = 6;
+    void func(
+        int i = [](int x) { return 3*x; }(x),
+        int j = [](int x) { return x; }(x)
+    )
+    { }
+
+    constexpr auto m = 
+        [](std::convertible_to<std::string_view> auto&& value) requires std::convertible_to<decltype(value), std::string_view>
+        {};
+
     void Run()
     {
+        auto ff = []<typename T>(){};
+        ff.operator()<int>();
+
         [](this auto&& self, int repetitions) -> void
         {
             std::println("Repetition {}", repetitions);
