f2cb2b04e5d6026b5602f129c0765a2546e343da
Vasilios Magriplis <etameson@gmail.com>
2024-05-31 05:53:28 +1000

    More messing with lambdas

--- src/MiscellaneousExperiments/randomstuff.cpp
+++ src/MiscellaneousExperiments/randomstuff.cpp
@@ -38,16 +38,54 @@ namespace DeducingThis
         [](std::convertible_to<std::string_view> auto&& value) requires std::convertible_to<decltype(value), std::string_view>
         {};
 
+    template<typename TFn, typename...TArgs, typename TExpected = std::expected<std::invoke_result_t<TFn, TArgs...>, std::exception>>
+    auto repeatable(int repetitions, TFn&& function, TArgs&&...args) -> TExpected
+    try
+    {
+        return std::invoke(std::forward<TFn>(function), std::forward<TArgs>(args)...);
+    }
+    catch (const std::exception& ex)
+    {
+        if (repetitions > 1)
+            return repeatable(--repetitions, function, std::forward<TArgs>(args)...);
+        return std::unexpected(ex);
+    }
+
+    auto repeatable2 = 
+        []<typename TFn, typename...TArgs, typename TExpected = std::expected<std::invoke_result_t<TFn, TArgs...>, std::exception>>(
+            this auto&& self, 
+            int repetitions, 
+            TFn&& function, 
+            TArgs&&...args
+        ) -> TExpected
+        {
+            try
+            {
+                return std::invoke(std::forward<TFn>(function), std::forward<TArgs>(args)...);
+            }
+            catch (const std::exception& ex)
+            {
+                if (repetitions > 1)
+                    return self(--repetitions, function, std::forward<TArgs>(args)...);
+                return std::unexpected(ex);
+            }
+        };
+
+    int sth(int x) { return x; }
+
     void Run()
     {
+        repeatable(5, sth, 1);
+        repeatable2(5, sth, 1);
+
         auto ff = []<typename T>(){};
         ff.operator()<int>();
 
-        [](this auto&& self, int repetitions) -> void
+        [](this auto&& self, int repetitions, auto&&...args) -> void
         {
             std::println("Repetition {}", repetitions);
             if (repetitions > 1)
-                self(--repetitions);
+                self(--repetitions, std::forward<decltype(args)>(args)...);
         }(5);
 
         scheduler sched{};
