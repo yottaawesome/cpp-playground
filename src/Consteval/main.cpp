aabbbee7bc5cca5090c82c1b45227fab2e3ad1be
Vasilios Magriplis <etameson@gmail.com>
2024-12-26 20:47:30 +1100

    Use correct line length formula.

--- src/Consteval/main.cpp
+++ src/Consteval/main.cpp
@@ -29,6 +29,27 @@ namespace Consteval1
 
 namespace Consteval2
 {
+    // std::sqrt is constexpr in C++26, which MSVC does not yet support.
+    // Adapted from https://gist.github.com/alexshtf/eb5128b3e3e143187794
+    constexpr double sqrtNewtonRaphson(double x, double curr, double prev)
+    {
+        return curr == prev
+            ? curr
+            : sqrtNewtonRaphson(x, 0.5 * (curr + x / curr), curr);
+    }
+
+    constexpr double SquareRoot(double x)
+    {
+        return x >= 0 && x < std::numeric_limits<double>::infinity()
+            ? sqrtNewtonRaphson(x, x, 0)
+            : std::numeric_limits<double>::quiet_NaN();
+    }
+
+    constexpr double Square(double x)
+    {
+        return x * x;
+    }
+
     struct Point
     {
         consteval Point(int x, int y)
@@ -43,30 +64,37 @@ namespace Consteval2
             : A(a), B(b) { }
         Point A;
         Point B;
+        consteval auto Length() noexcept -> float
+        {
+            // rise over run
+            double rise = Square(static_cast<double>(B.Y - A.Y));
+            double run = Square(static_cast<double>(B.X - A.X));
+            return static_cast<float>(SquareRoot(rise + run));
+        }
     };
 
     constexpr auto GetLines() -> std::vector<Line>
     {
         std::vector<Line> p;
-        p.push_back({{ 1, 2 }, { 2, 3 }});
+        p.push_back({{ 0, 0 }, { 3, 0 }});
         return p;
     }
 
-    consteval auto GetTotalLength() -> std::uint32_t
+    consteval auto GetTotalLength() -> float
     {
-        std::uint32_t length = 0;
         auto lines = GetLines();
         if (lines.empty())
             throw std::exception("Cannot be empty.");
-        for (Line l : lines) // rise over run
-            length += (l.B.Y - l.A.Y) / (l.B.X - l.A.X);
+        float length = 0;
+        for (Line l : lines)
+            length += l.Length();
         return length;
     }
 
     auto Run() -> void
     {
-        constexpr int totalLength = GetTotalLength();
-        std::println("The length is {}.", totalLength);
+        constexpr float totalLength = GetTotalLength();
+        std::println("The computed linear line length of all lines is {} units.", totalLength);
     }
 }
 
