76fe8b158303e85fbf37837d2e35d786b2b1d024
Vasilios Magriplis <etameson@gmail.com>
2025-05-08 22:52:59 +1000

    Minor changes.

--- src/EventPropagation/main.cpp
+++ src/EventPropagation/main.cpp
@@ -7,7 +7,7 @@ namespace Events
         template<typename T>
         auto M(this T self)
         {
-            if constexpr (requires (const T & t) { t.Blah(); })
+            if constexpr (requires (T t) { t.Blah(); })
                 return self.Blah();
             else
                 return self.Default();
@@ -26,6 +26,11 @@ namespace Events
             return 10;
         }
     };
+
+    void J(auto y)
+    {
+        std::println("{}", y.M());
+    }
 
     struct SomeEvent { int X = 0; };
     struct SomeOtherEvent {};
@@ -50,40 +55,47 @@ namespace Events
     {
         std::tuple<std::vector<Ts>...> Tuples{ std::vector<Ts>{}... };
 
-        void AddHandler(OneOf<Ts...> auto&& handle)
+        void AddHandler(this auto& self, OneOf<Ts...> auto&& handle)
         {
-            Get<decltype(handle)>().push_back(handle);
+            self.Get<decltype(handle)>().push_back(handle);
         }
 
         template<typename T>
-        auto& Get()
+        auto& Get(this auto& self)
         {
-            return std::get<std::vector<std::remove_cvref_t<T>>>(Tuples);
+            return std::get<std::vector<std::remove_cvref_t<T>>>(self.Tuples);
         }
 
-        void Raise(auto&& event)
+        void Raise(this auto& self, auto&& event)
         {
-            ([this]<typename M = Ts>(auto&& event)
+            ([]<typename M = Ts>(auto& self, auto&& event)
             {
                 if constexpr (requires (M m) { m.Handle(event); })
                 {
-                    auto& handlers = Get<M>();
+                    auto& handlers = self.Get<M>();
                     for (auto&& handler : handlers)
                         handler.Handle(event);
                 }
-            }(event), ...);
+            }(self, event), ...);
         }
     };
 
+    template<typename...Ts>
+    struct Registrations<std::tuple<Ts...>> : Registrations<Ts...> { };
 
     void Run()
     {
+        X x;
+        J(x);
+
         std::tuple AA{ HandlerA{} } ;
         std::tuple BB{ HandlerB{} };
         auto M = std::tuple_cat(AA, BB);
+
         Registrations<decltype(M)> registrationsA;
-
-
+        registrationsA.AddHandler(HandlerA{});
+        registrationsA.AddHandler(HandlerB{});
+        registrationsA.Raise(SomeEvent{ 12 });
 
         Registrations<HandlerA, HandlerB> registrations;
         registrations.AddHandler(HandlerA{});
@@ -91,7 +103,6 @@ namespace Events
         registrations.Raise(SomeEvent{12});
     }
 }
-
 
 int main()
 {
