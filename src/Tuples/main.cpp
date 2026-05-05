a9d57be102a1c702f3be8367749ba4c1d518c200
Vasilios Magriplis <etameson@gmail.com>
2026-05-05 20:28:43 +1000

    Minor changes.

--- src/Tuples/main.cpp
+++ src/Tuples/main.cpp
@@ -4,16 +4,62 @@ import tupletoparameterpack;
 import variantmadness;
 import moremadness;
 
-constexpr bool something()
+template<typename T>
+struct Function {};
+
+template<typename Ret, typename...Args>
+struct Function<Ret(*)(Args...)>
 {
-	return false;
+	using FnType = Ret(*)(Args...);
+	inline auto operator()(this auto& self, Args&&... args)
+	{
+		return self.Invoke(std::forward<Args>(args)...);
+		//return FnType(std::forward<Args>(args)...);
+	}
+
+	inline auto Invoke(this auto&& self, Args&&... args)
+	{
+		
+	}
+};
+
+template<auto Fn>
+struct Function2 : Function<decltype(Fn)> 
+{
+	using FnType = Function<decltype(Fn)>::FnType;
+
+	inline auto Invoke(this auto&& self, auto&&... args)
+	{
+		return Fn(std::forward<decltype(args)>(args)...);
+	}
+
+	/*static inline auto operator()(auto&&... args)
+	{
+		return std::invoke(Fn, std::forward<decltype(args)>(args)...);
+	}*/
+	//constexpr static auto FnType Invoker = Fn;
+
+};
+
+template<auto VFn>
+auto inline Fn(auto&&... args)
+{
+	return VFn(std::forward<decltype(args)>(args)...);
 }
 
-template<bool b = something()>
-struct M {};
+void SomeFunction(int X)
+{
+
+}
+
+constexpr auto SomeFunctionPtr1 = Function<decltype(&SomeFunction)>{};
+constexpr auto SomeFunctionPtr = Function2<SomeFunction>{};
 
 int main()
 {
+	SomeFunctionPtr(5);
+	//SomeFunctionPtr.Invoker(5);
+
 	TupleCats::Run();
 	Structs::Run();
 	PrettyPrint::Run();
